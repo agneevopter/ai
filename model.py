c451fb790acbafdc694b296aa8ea51ce648215cd
agneevopter <amaitra990@gmail.com>
2024-11-26 17:44:55 +0530

    first ai code

--- model.py
+++ model.py
@@ -40,4 +40,50 @@ print(dataset)
 # but in Jupyter notebooks, we simply write the name
 # of the variable and it is printed nicely 
 #dataset
+# Load a library to do the hard work for us
+import statsmodels.formula.api as smf
 
+# First, we define our formula using a special syntax
+# This says that boot_size is explained by harness_size
+formula = "boot_size ~ harness_size"
+
+# Create the model, but don't train it yet
+model = smf.ols(formula = formula, data = dataset)
+
+# Note that we have created our model but it does not 
+# have internal parameters set yet
+if not hasattr(model, 'params'):
+    print("Model selected but it does not have parameters set. We need to train it!")
+# Train (fit) the model so that it creates a line that 
+# fits our data. This method does the hard work for
+# us. We will look at how this method works in a later unit.
+fitted_model = model.fit()
+
+# Print information about our model now it has been fit
+print("The following model parameters have been found:\n" +
+        f"Line slope: {fitted_model.params[1]}\n"+
+        f"Line Intercept: {fitted_model.params[0]}")
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+# Show a scatter plot of the data points and add the fitted line
+# Don't worry about how this works for now
+plt.scatter(dataset["harness_size"], dataset["boot_size"])
+plt.plot(dataset["harness_size"], fitted_model.params[1] * dataset["harness_size"] + fitted_model.params[0], 'r', label='Fitted line')
+
+# add labels and legend
+plt.xlabel("harness_size")
+plt.ylabel("boot_size")
+plt.legend()
+plt.show()
+plt.savefig('interactive_plot.png')
+# harness_size states the size of the harness we are interested in
+harness_size = { 'harness_size' : [4] }
+
+# Use the model to predict what size of boots the dog will fit
+approximate_boot_size = fitted_model.predict(harness_size)
+
+# Print the result
+print("Estimated approximate_boot_size:")
+print(approximate_boot_size[0])
